2e67abc7f26173542e048778ce1763813251b5a3
Raúl Conde <106599244+RaulLeopard@users.noreply.github.com>
2023-06-19 16:09:32 +0200

    Add files via upload

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -16,5 +16,45 @@
 
 void    ft_sort_int_tab(int *tab, int size)
 {
-    
+    int a;
+    int b;
+    int c;
+    int temp;
+
+    for (a = 0; a < size - 1; a++)
+    {
+        c = a;
+
+        for (b = a + 1; b < size; b++)
+        {
+            if (tab[b] < tab [c])
+            {
+                c = b;
+            }
+        }
+        temp = tab[a];
+        tab[a] = tab[c];
+        tab[c] = temp;
+    }
 }
+
+/*int main() 
+{
+    int tab[] = {5, 2, 9, 1, 3};
+    int size = sizeof(tab) / sizeof(tab[0]);
+    int i;
+
+    printf("Arreglo original: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", tab[i]);
+    }
+
+    ft_sort_int_tab(tab, size);
+
+    printf("\nArreglo ordenado en orden ascendente: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", tab[i]);
+    }
+
+    return 0;
+}*/
